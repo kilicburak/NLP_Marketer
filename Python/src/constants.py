60aca3bf082dab9911c431e4db316db0bd72a9c5
Burak Kilic <burak@biglelegal.com>
2020-09-09 04:14:34 +0200

    Add constant variables for error definitions.

---
+++ Python/src/constants.py
@@ -0,0 +1,6 @@
+######################## Errors ##########################
+PAYLOAD_NOT_FOUND_ERROR = 'Payload can not be found.'
+KEYWORD_NOT_FOUND_ERROR = "Keywords can to be empty."
+TYPE_NOT_CORRECT_ERROR = 'Payload type is incorrect, please upload text.'
+MODEL_NOT_FOUND_ERROR = 'The ML model not found. (default name: en_core_web_sm)'
+DATA_MODEL_ERROR = 'Data model, text or source ML model has some problems, please check.'
